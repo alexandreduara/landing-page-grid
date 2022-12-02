b4259b0d08ed97b31f84043057561d3b079ef05c
Alexandre Duarte $ git config --global user.email alexandreduara@gmail.comgit config --global user.name Alexandre <alexandreduara@gmail.com>
2022-12-02 18:41:33 -0300

    adicionando o menu usando JS e responsividade

---
+++ index.js
@@ -0,0 +1,13 @@
+const menu__button = document.querySelector(".menu__button");
+const menu = document.querySelector(".menu__lateral");
+const menu__image = document.querySelector(".menu__button--image");
+
+menu__button.addEventListener('click', ()=>{
+    menu.classList.toggle('menu__lateral--active');
+    if (menu__image.getAttribute('src') == './images/icon-hamburger.svg'){
+        menu__image.setAttribute('src','./images/icon-close.svg');
+    }
+    else{
+        menu__image.setAttribute('src', './images/icon-hamburger.svg');
+    }
+})
